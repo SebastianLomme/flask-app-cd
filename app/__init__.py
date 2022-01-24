446bcc476ee13a62ac58adda8be2d27f8226d7b5
Sebastian Lomme <sebastian.lomme@gmail.com>
2022-01-24 10:18:57 +0100

    wheel added to requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ login_manager.login_view = "login"
 login_manager.login_message_category = "info"
 
 def create_app(config_class=Config):
-    app = Flask(__name__, template_folder='templates')
+    app = Flask(__name__)
     app.config.from_object(config_class)
     db.init_app(app)
     bcrypt.init_app(app)
